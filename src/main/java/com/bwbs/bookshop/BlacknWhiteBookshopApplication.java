b03cfad1fdc23786e9d54838c25d1da9f9a0554a
맹주희 <juhee@MacBook.local>
2025-04-21 22:30:36 +0900

    Initial commit

---
+++ src/main/java/com/bwbs/bookshop/BlacknWhiteBookshopApplication.java
@@ -0,0 +1,13 @@
+package com.bwbs.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlacknWhiteBookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlacknWhiteBookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwbs/bookshop/BlacknWhiteBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwbs.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacknWhiteBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
