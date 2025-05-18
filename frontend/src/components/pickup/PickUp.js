06107ab9c456551c54ff28a0912f1aa9632a2ebc
maengjuhee <juheemaeng@naver.com>
2025-05-19 03:34:27 +0900

    바로픽업 페이지 구성 수정

--- frontend/src/components/pickup/PickUp.js
+++ frontend/src/components/pickup/PickUp.js
@@ -7,33 +7,70 @@ const PickUp=()=>{
                 <div className="container">
                     <div className="top">
                         <h3>바로픽업</h3>
+                        <div className="process">
+                            <p><span>1</span> 장바구니</p>
+                            <p><span>2</span> 주문/결제</p>
+                            <p><span>3</span> 주문완료</p>
+                        </div>
                     </div>
                     <div className="content">
                         <div className="left">
                             <div className="pickup_info">
                                 <h3>픽업 정보</h3>
                                 <ul>
-                                    <li><strong>픽업 가능일</strong><span>2025년 5월 1일</span></li>
-                                    <li><strong>방문할 매장</strong><span>분당점</span></li>
-                                    <li><strong>선택 유형</strong><span>바로픽업존에서 받기</span></li>
+                                    <li>픽업 가능일<span><input type="date"/>
+                                        <select>
+                                            <option>10:00</option>
+                                            <option>12:00</option>
+                                            <option>14:00</option>
+                                            <option>16:00</option>
+                                            <option>18:00</option>
+                                            <option>20:00</option>
+                                        </select></span>
+                                    </li>
+                                    <li>픽업 매장<span>흑백책방 홍대점</span></li>
+                                    <li>수령 방법<span>바로픽업</span></li>
+                                    <li>수령인<span>맹주희 / 010-1234-1234</span></li>
+                                    <li className="wrap-row">
+                                        포장 옵션
+                                        <span className="wrap-option">
+                                            <label htmlFor="wrap">포장 서비스를 이용합니다. (+100원)</label>
+                                            <input type="checkbox" id="wrap" />
+                                        </span>
+                                    </li>
                                 </ul>
-                            </div>
-                            <div className="member_info">
-                                <h3>주문자 정보</h3>
-                                <div className="row">
-                                    <span>고홍시 / 010-1234-1234</span>
-                                    <button type="button" className="changeBtn">변경</button>
-                                </div>
                             </div>
                             <div className="order">
                                 <h3>주문 상품</h3>
                                 <div className="row">
-                                    <img src="/img/ex1.jpg" alt="책 썸네일" />
-                                    <div className="product">
-                                        <p className="title">단 한 번의 삶</p>
-                                        <p className="option">바로픽업 / 1개</p>
-                                        <p className="price">15,120원 <span className="origin">16,800원</span></p>
+                                    <div className="thumb">
+                                        <img src="/img/ex1.jpg" alt="책 썸네일" />
                                     </div>
+                                    <div className="title">단 한 번의 삶</div>
+                                    <div className="origin">16,800원</div>
+                                    <div className="price">15,120원</div>
+                                    <div className="qty">1</div>
+                                    <div className="t_price">15,120원</div>
+                                </div>
+                                <div className="row">
+                                    <div className="thumb">
+                                        <img src="/img/ex2.jpg" alt="책 썸네일" />
+                                    </div>
+                                    <div className="title">듀얼 브레인</div>
+                                    <div className="origin">16,800원</div>
+                                    <div className="price">15,120원</div>
+                                    <div className="qty">1</div>
+                                    <div className="t_price">15,120원</div>
+                                </div>
+                                <div className="row">
+                                    <div className="thumb">
+                                        <img src="/img/ex3.jpg" alt="책 썸네일" />
+                                    </div>
+                                    <div className="title">100가지의 필사노트</div>
+                                    <div className="origin">16,800원</div>
+                                    <div className="price">15,120원</div>
+                                    <div className="qty">1</div>
+                                    <div className="t_price">15,120원</div>
                                 </div>
                             </div>
                         </div>
@@ -55,21 +92,13 @@ const PickUp=()=>{
                                     <span>상품 할인</span>
                                     <span>-2,200원</span>
                                 </div>
-                                <div className="row">
-                                    <span>포인트 사용</span>
-                                    <span>0원</span>
-                                </div>
                                 <div className="total">
-                                    <strong>결제 금액</strong>
+                                    <span>결제 예정 금액</span>
                                     <strong>0원</strong>
                                 </div>
                                 <div className="point">
                                     <span>적립 예정 포인트</span>
                                     <span>1,100P</span>
-                                </div>
-                                <div className="agreement">
-                                    <input type="checkbox" id="agree" />
-                                    <label htmlFor="agree">&nbsp;위 주문내용을 확인하였으며, 결제에 동의합니다.</label>
                                 </div>
                                 <button type="button" className="payBtn">결제하기</button>
                             </div>
