ce757c752b048dccb460a5898a510c26318d6caf
zerovin <amorfati9021@gmail.com>
2025-05-24 01:21:52 +0900

    결제완료 페이지 수정

--- frontend/src/components/cart/CartPaycomplete.js
+++ frontend/src/components/cart/CartPaycomplete.js
@@ -6,6 +6,14 @@ const CartPaycomplete=()=>{
         <Fragment>
             <div id="paycomplete">
                 <div className="container">
+                    <div className="top">
+                        <h3>주문/결제</h3>
+                        <div className="process">
+                            <p><span>1</span> 장바구니</p>
+                            <p><span>2</span> 주문/결제</p>
+                            <p><span>3</span> 주문완료</p>
+                        </div>
+                    </div>
                     <p className="success">결제가 성공적으로 완료되었습니다 🎉</p>
                     <div className="after_btn">
                         <Link to="/book/list/all">계속 둘러보기</Link>
